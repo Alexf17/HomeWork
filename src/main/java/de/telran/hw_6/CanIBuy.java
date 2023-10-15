4c23042863932190e00a0f5d44955b62aeceee9b
Oleksii <achili3f17@gmail.com>
2023-10-15 23:01:53 +0200

    update code

--- src/main/java/de/telran/hw_6/CanIBuy.java
+++ src/main/java/de/telran/hw_6/CanIBuy.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class CanIBuy {
     public static void main(String[] args) {
-        /** Создайте две переменные isEdekaOpen и isReweOpen, значения которых зависят от того, открыты магазины или нет.
+        /* Создайте две переменные isEdekaOpen и isReweOpen, значения которых зависят от того, открыты магазины или нет.
          Реализует логический метод canBuy, возвращающий boolean
          Значение этой переменной должно быть true, если хотя бы один магазин открыт, иначе false.
          Отобразите строку «Я могу купить еду, это ……» и значение.*/
@@ -12,14 +12,15 @@ public class CanIBuy {
         Random random = new Random();
         int num = random.nextInt(2);
 
-        boolean isEdekaOpen = (num == 0) ? false : true;
-        ;
+        boolean isEdekaOpen = num != 0;
+
         boolean isReweOpen = !isEdekaOpen;
         System.out.println("Я могу купить еду,в " + (isEdekaOpen ? "Edeka ! " : "ReWe! ") + "и это " + canBuy(isReweOpen, isEdekaOpen));
 
     }
 
-    /** В данном случае метод и не нужен, так как логика основана на рандоме, но по заданию нужен метод */
+    /** В данном случае метод и не нужен, так как логика основана на рандоме, но если переписать на получение данных со
+     сканера то метод будет работать */
     public static boolean canBuy(boolean isEdekaOpen, boolean isReweOpen) {
         if (isEdekaOpen || isReweOpen) {
             return true;
