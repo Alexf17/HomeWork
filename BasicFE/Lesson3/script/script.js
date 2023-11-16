893aa0e7cb839068b1cceb07d76d976849bb5fae
Oleksii <achili3f17@gmail.com>
2023-11-16 01:39:30 +0100

    HW6

---
+++ BasicFE/Lesson3/script/script.js
@@ -0,0 +1,36 @@
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 !== 0) {
+//     console.log(arr[i]);
+//   }
+// }
+
+// let arr = [2, 5, 18, 24, 2, 7, 16, 78, 15, -7];
+// let sumEven = 0;
+// let sumNotEven = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 == 0) {
+//     sumEven += arr[i];
+//   } else if (arr[i] % 2 !== 0) {
+//     sumNotEven += arr[i];
+//   }
+// }
+// console.log(sumEven);
+// console.log(sumNotEven);
+
+// if (sumEven < sumNotEven) {
+//   console.log(sumNotEven - sumEven);
+// } else {
+//   console.log(sumEven - sumNotEven);
+// }
+
+// let d = 7;
+// let t = 3;
+// for (let i = 0; i < t; i++) {}
+let d = 7;
+let result = 7;
+
+for (let i = 1; i <= 3; i++) {
+  console.log(result);
+  result *= d;
+}
